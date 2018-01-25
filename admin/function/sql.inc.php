26e51a6230594e085bf3ac76cdbf4a26de78dfed
Roman Kocian <roman.kocian@gmail.com>
2018-01-25 12:09:22 +0100

    Create sql.inc.php

---
+++ admin/function/sql.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+// GENERAL SETTING SMTP EMAIL FOR ORDERS
+function generalSetting($controller)
+{
+  require('../config.php');
+  $stmt = $mysqli->prepare('SELECT `value` FROM '. DB_TABLE_SETTING .' WHERE `controller` = ?');
+  $stmt->bind_param("s", $controller);
+  $stmt->execute();
+  $stmt->bind_result($value);
+  $stmt->fetch();
+  $stmt->close();
+  $mysqli->close();
+  return htmlspecialchars($value);
+}
